8aa4f910583d23e819f4db6e94f5d571894323d3
Admor Aloysious Aguilar <wishfuldroplet@gmail.com>
2018-10-23 10:44:40 +0800

    * Demonstration of how to inherit from derived Actor classes in compliance with Unity's serialization system.

--- Assets/_Project/Actors/LevelItemActor/LevelItemActor.cs
+++ Assets/_Project/Actors/LevelItemActor/LevelItemActor.cs
@@ -1,18 +1,47 @@
 ﻿using UnityEngine;
+using System;
 using WishfulDroplet;
 
 
-public class LevelItemActor : Actor<LevelItemActor, LevelItemActor.LevelItemBrain> {
-    [InspectorNote("Level Item Actor")]
-    [Header("Data")]
-    public int test;
-
-
-    private void OnDrawGizmos() {
-        
-    }
-
+public class LevelItemActor : LevelItemActor<LevelItemActor, LevelItemActor.LevelItemBrain> {
     public abstract class LevelItemBrain : ActorBrain<LevelItemActor> {
-        
+
     }
 }
+
+
+public class LevelItemActor<T, U> : Actor<T, U> 
+    where T : Actor<T, U>
+    where U : ActorBrain<T> {
+    [InspectorNote("Level Item Actor")]
+    public int test = 0;
+}
+
+
+
+//public class LevelItemActor : Actor<LevelItemActor, LevelItemActor.LevelItemBrain> {
+//    [InspectorNote("Level Item Actor")]
+//    [Header("Data")]
+//    public float moveSpeed;
+
+
+//    [Serializable]
+//    public abstract class LevelItemBrain : ActorBrain<LevelItemActor> {
+
+//    }
+
+
+//    [Serializable]
+//    public abstract class LevelItemState : State<LevelItemActor> {
+//        public override void DoEnter(LevelItemActor owner) {}
+//        public override void DoExit(LevelItemActor owner) {}
+//    }
+
+
+//    public class MoveStates {
+//        [Serializable]
+//        public class Move : LevelItemState {
+
+//        }
+//    }
+//}
